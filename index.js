f7fcc05a8894f4800ec78a70c9f74fd52ef01a1f
Sanjeeva Rao <snowgrc3133@gmail.com>
2026-08-12 12:08:09 +0530

    jsonplaceholder get request api

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
- let a=10,b=20;
- console.log(a+b);
-console.log("hello);
+
+//Program 1
+// async function getPost(){
+//      let response=await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//      console.log("status",response.status);
+//      let data= await response.json();
+//       console.log("title",data.title);
+// }
+// getPost();
+
+ //Program 2
+  async function getPost(){
+     let response=await fetch('https://jsonplaceholder.typicode.com/posts/1');
+      if(response.status=='200'){
+        console.log("successfull")
+      }else{
+        console.log("failed");
+      }
+  }
+  getPost();
